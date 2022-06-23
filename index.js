7cc018ba4ab7ab3311965a6f6a9254a920592bd1
Betsy <betsy.griffith12@gmail.com>
2022-06-22 21:40:41 -0400

    added promptUser function with a prompt array containing question objects to ask the user to receive their input

--- index.js
+++ index.js
@@ -2,16 +2,66 @@ const inquirer = require('inquirer');
 // const fs = require('fs');
 // const generatePage = require('./src/page-template.js');
 
-inquirer
-.prompt([
-    {  
-    type: 'input',
-    name: 'name',
-    message: 'What is the name of your project?'
-    }
-])
-.then(answers => console.log(answers));
+const promptUser = () => {
+    return inquirer
+    .prompt([
+        {  
+            type: 'input',
+            name: 'title',
+            message: 'What is the title of your project?'
+        },
+        {
+            type: 'input',
+            name: 'description',
+            message: 'Add a description for your project.'
+        },
+        {
+            type: 'checkbox',
+            name: 'contents',
+            message: 'Choose which items your README will include in your Table of Contents. (Check all that apply)',
+            choices: ['Installation', 'Usage', 'License', 'Contributing', 'Tests', 'Questions']
+        },
+        {
+            type: 'input',
+            name: 'installation',
+            message: 'Add installation instructions for future users.'
+        },
+        {
+            type: 'input',
+            name: 'usage',
+            message: 'Add information about your project allowing future readers to know how your project is intended to be used.'
+        },
+        {
+            type: 'checkbox',
+            name: 'license',
+            message: 'What license (if any) does your project have?',
+            choices: ['MIT', 'Apache 2.0', 'ISC']
+        },
+        {
+            type: 'input',
+            name: 'contributing',
+            message: 'Detail any guidelines to follow if you would like other developers to contribute to your project.'
+        },
+        {
+            type: 'input',
+            name: 'test',
+            message: 'detail how others can test your application.'
+        },
+        {
+            type: 'input',
+            name: 'questions',
+            message: 'Type your GitHub username to include a link to your profile and your email to have other users reach out to you with questions.'
+        },
+        {
+            type: 'confirm',
+            name: 'confirmREADME',
+            message: 'Please confirm you have selected all items you intend to include in your README.',
+            default: false
+        }
+    ]);
+};
 
+promptUser().then(answers => console.log(answers));
 
 
 
